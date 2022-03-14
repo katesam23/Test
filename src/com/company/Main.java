9a599230d165ab9ea2525eda9696d76cf0220baf
Tixon123 <84869623+Tixon123@users.noreply.github.com>
2022-03-14 19:12:51 +0400

    Revert "m1 merged into m"
    This reverts commit ddeb0486438ba9e847c4fc396106a83d2ce98d04.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("master2 commit");
+        System.out.println("master");
     }
 }
